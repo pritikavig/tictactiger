0e2f147ac4f2ff391f1fe24f0b50f092d0dcc132
pritikavig <pritikavig@aol.com>
2016-10-13 08:15:46 -0400

    remove unnecessary files from template

--- TongueTiedTiger/app/controllers/HomeController.scala
+++ TongueTiedTiger/app/controllers/HomeController.scala
@@ -1,24 +1,45 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
+/*
+
+  Supported actions:
+
+  /ttt @player <- if a game doesn't already exist in the channel, create one
+
+  /ttt game <- show current game & turn in channel
+
+  /ttt gameover <- clear current game in channel
+
+  /ttt move <move> <- attempt to make a move for the player
+
+
  */
+
 @Singleton
 class HomeController @Inject() extends Controller {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action { request =>
+
+    /* --- Hacky Slack Client -- my apologies ---- */
+    val token: Option[String] = request.body.asFormUrlEncoded.flatMap(m => m.get("token").flatMap(_.headOption))
+    val teamId: Option[String] = request.body.asFormUrlEncoded.flatMap(m => m.get("team_id").flatMap(_.headOption))
+    val teamDomain: Option[String] = request.body.asFormUrlEncoded.flatMap(m => m.get("team_domain").flatMap(_.headOption))
+    val channelId: Option[String] = request.body.asFormUrlEncoded.flatMap(m => m.get("channel_id").flatMap(_.headOption))
+    val channelName: Option[String] = request.body.asFormUrlEncoded.flatMap(m => m.get("channel_name").flatMap(_.headOption))
+    val userId: Option[String] = request.body.asFormUrlEncoded.flatMap(m => m.get("user_id").flatMap(_.headOption))
+    val username: Option[String] = request.body.asFormUrlEncoded.flatMap(m => m.get("user_name").flatMap(_.headOption))
+    val command: Option[String] = request.body.asFormUrlEncoded.flatMap(m => m.get("text").flatMap(_.headOption))
+
+    Ok(request.body.toString)
+
   }
+
+}
+
+
+object Command extends Enumeration {
 
 }
